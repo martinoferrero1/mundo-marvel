fc4e517a9c7de90e658be5d50f47c25806334357
martinoferrero1 <martinoferrero02@gmail.com>
2024-08-16 03:55:49 -0300

    Fetching de personakes

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { Character } from './types';
+import './App.css';
+
+const HASH = `4dfaa6f4e5cb993da155037080ba9b54`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=1&apikey=c8ab8d276656753ee4b88c8234f79a08&hash=${HASH}&limit=10`)
+      .then(async res => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        return await res.json();
+      })
+      .then(res => {
+        setCharacters(res.data.results);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.error(err);
+        setError('Failed to fetch data');
+        setLoading(false);
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className='App'>
+      {loading && <strong>Cargando...</strong>}
+      {error && <strong>{error}</strong>}
+      {characters.length > 0 && (
+        <table style={{width : '100%'}}>
+          <thead>
+            <th>Nombre</th>
+            <th>Imagen</th>
+          </thead>
+          <tbody>
+          {characters.map(character => (
+            <div key={character.id}>
+              <span className='characters--row'>
+                <td>{character.name}</td>
+                <img src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt={character.name} width={'100 px'}/>
+              </span>
+            </div>
+          ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
